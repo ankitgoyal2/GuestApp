ba9bb3b08c7db21fcc0a4417b2e49c90c0cafe4f
Sudhanshu Gupta <sudhanshu10101999@gmail.com>
2020-06-25 15:05:50 +0530

    Shimmer Effect Added

--- app/src/main/java/com/example/guestapp/DetailActivity.java
+++ app/src/main/java/com/example/guestapp/DetailActivity.java
@@ -2,19 +2,16 @@ package com.example.guestapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.transition.Fade;
 import android.view.View;
-import android.view.Window;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.guestapp.model.Machine;
-import com.google.firebase.auth.FirebaseAuth;
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,12 +26,21 @@ public class DetailActivity extends AppCompatActivity {
     Machine machine;
     TextView company, type, model, serialNo, dateOfInstallation, price;
     Button feedbackButton;
+    ShimmerFrameLayout shimmerFrameLayout;
+    ConstraintLayout constraintLayoutBox1, constraintLayoutBox2, constraintLayoutBox3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        shimmerFrameLayout = findViewById(R.id.shimmer_container);
+        constraintLayoutBox1 = findViewById(R.id.constraint_layout_box_1);
+        constraintLayoutBox2 = findViewById(R.id.constraint_layout_box_2);
+        constraintLayoutBox3 = findViewById(R.id.constraint_layout_box_3);
 
+        constraintLayoutBox1.setVisibility(View.INVISIBLE);
+        constraintLayoutBox2.setVisibility(View.INVISIBLE);
+        constraintLayoutBox3.setVisibility(View.INVISIBLE);
 
         company = findViewById(R.id.company);
         type = findViewById(R.id.type);
@@ -59,6 +65,11 @@ public class DetailActivity extends AppCompatActivity {
                 serialNo.setText(machine.getSerialNumber());
                 price.setText(String.valueOf(machine.getPrice()));
                 dateOfInstallation.setText(machine.getDateOfInstallation());
+                shimmerFrameLayout.stopShimmer();
+                shimmerFrameLayout.setVisibility(View.GONE);
+                constraintLayoutBox1.setVisibility(View.VISIBLE);
+                constraintLayoutBox2.setVisibility(View.VISIBLE);
+                constraintLayoutBox3.setVisibility(View.VISIBLE);
             }
 
             @Override
@@ -77,5 +88,17 @@ public class DetailActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.feedback_enter_anim,R.anim.fade_out);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        shimmerFrameLayout.startShimmer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        shimmerFrameLayout.stopShimmer();
     }
 }
